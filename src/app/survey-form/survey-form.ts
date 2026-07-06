206c2434859d018879873ed044e489472522566c
danielkrafzik <daniel.krafzik@hotmail.de>
2026-07-06 20:09:29 +0200

    created question-form component and integrated the component in survey-form

--- src/app/survey-form/survey-form.ts
+++ src/app/survey-form/survey-form.ts
@@ -4,10 +4,11 @@ import { NgFor, NgClass, CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
+import { QuestionForm } from '../question-form/question-form';
 
 @Component({
   selector: 'app-survey-form',
-  imports: [CommonModule, FormsModule, RouterLink],
+  imports: [CommonModule, FormsModule, RouterLink, QuestionForm],
   templateUrl: './survey-form.html',
   styleUrl: './survey-form.scss',
 })
