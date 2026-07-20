3b2bed49abe5c63eebc4b480c75d2880d732b836
danielkrafzik <daniel.krafzik@hotmail.de>
2026-07-20 20:06:08 +0200

    implemented add answer function

--- src/app/question-form/question-form.ts
+++ src/app/question-form/question-form.ts
@@ -1,9 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-question-form',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './question-form.html',
   styleUrl: './question-form.scss',
 })
-export class QuestionForm {}
+export class QuestionForm {
+
+  answers = [
+    { letter: 'A', text: '' },
+    { letter: 'B', text: '' }
+  ];
+
+  readonly letters = ['A', 'B', 'C', 'D', 'E', 'F'];
+
+  addAnswer() {
+    if (this.answers.length >= 6) {
+      return;
+    }
+
+    this.answers.push({
+      letter: this.letters[this.answers.length],
+      text: ''
+    });
+  }
+
+  removeAnswer(index: number) {
+    if (this.answers.length <= 2) {
+      return;
+    }
+
+    this.answers.splice(index, 1);
+
+    this.answers.forEach((answer, i) => {
+      answer.letter = this.letters[i];
+    });
+  }
+}
